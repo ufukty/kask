b19e6683f6105f28b07aecc361fb33d023f57914
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-04-25 23:21:27 +0300

    int/builder: fixes sitemap generation. adds dir-node mapping. alters `TemplateContent` typedef. fixes syntax errors on `execDir`

--- internal/builder/build_test.go
+++ internal/builder/build_test.go
@@ -5,23 +5,43 @@ import (
 	"os"
 	"testing"
 
-	"github.com/ufukty/kask/internal/compiler/builder/directory"
+	"github.com/ufukty/kask/internal/builder/markdown"
 )
 
-func TestBuild(t *testing.T) {
-	dir, err := directory.Inspect("testdata/acme")
-	if err != nil {
-		t.Fatal(fmt.Errorf("prep, directory.Inspect: %w", err))
-	}
+var defaults = Args{
+	Domain: "http://localhost:8080",
+	Dev:    false,
+}
 
+func TestBuild(t *testing.T) {
 	tmp, err := os.MkdirTemp(os.TempDir(), "kask-test-build-*")
 	if err != nil {
 		t.Fatal(fmt.Errorf("os.MkdirTemp: %w", err))
 	}
-
 	fmt.Println("temp folder:", tmp)
 
-	err = Build(tmp, dir, &Args{Domain: "http://localhost:8080"})
+	err = Build(tmp, "testdata/acme", defaults)
+	if err != nil {
+		t.Fatal(fmt.Errorf("act, Build: %w", err))
+	}
+}
+
+func TestBuilder(t *testing.T) {
+	tmp, err := os.MkdirTemp(os.TempDir(), "kask-test-build-*")
+	if err != nil {
+		t.Fatal(fmt.Errorf("os.MkdirTemp: %w", err))
+	}
+	fmt.Println("temp folder:", tmp)
+
+	b := builder{
+		args:          defaults,
+		assets:        []string{},
+		stylesheets:   map[string]string{},
+		pagesMarkdown: map[string]*markdown.Page{},
+	}
+
+	err = b.Build(tmp, "testdata/acme")
+
 	if err != nil {
 		t.Fatal(fmt.Errorf("act, Build: %w", err))
 	}
