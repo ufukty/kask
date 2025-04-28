dcd32e2d46826a3b71fce8975d6ac790aa291183
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-04-28 12:45:14 +0300

    dev/build-docs: fixes dev branch/dir name ambiguity in checkout command

--- dev/build-docs.sh
+++ dev/build-docs.sh
@@ -6,5 +6,5 @@ git fetch --tags --quiet
 git checkout "$(git tag --list 'v*' | sort -Vr | head -n 1)"
 go install ./cmd/kask
 
-git checkout dev
+git checkout dev --
 kask build -in docs -out docs-build -domain / -v
