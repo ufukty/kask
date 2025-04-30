577f9581962306779d68a0a653a38ef93f329708
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-04-30 12:37:43 +0300

    dev/cloudflare/pages: moves build-docs script in and switches to `make` to build kask, which embed version number correctly

--- dev/cloudflare/pages/build-docs.sh
+++ dev/cloudflare/pages/build-docs.sh
@@ -4,7 +4,7 @@ set -ve
 
 git fetch --tags --quiet
 git checkout "$(git tag --list 'v*' | sort -Vr | head -n 1)"
-go install ./cmd/kask
+make install
 
 git checkout dev --
-$(go env GOPATH)/bin/kask build -in docs -out docs-build -domain / -v
+~/bin/kask build -in docs -out docs-build -domain / -v
