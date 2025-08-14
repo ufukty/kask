89e381a077baf9eb0ae525bd51048704957787c9
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-08-14 12:19:57 +0300

    dev/cf/pages/build-docs.sh: fixes the outdated branch name

--- dev/cloudflare/pages/build-docs.sh
+++ dev/cloudflare/pages/build-docs.sh
@@ -6,5 +6,5 @@ git fetch --tags --quiet
 git checkout "$(git tag --list 'v*' | sort -Vr | head -n 1)"
 make install
 
-git checkout dev --
+git checkout main --
 ~/bin/kask build -in docs -out docs-build -domain / -v
