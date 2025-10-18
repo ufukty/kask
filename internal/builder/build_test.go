3e75019f30f9e85a4878e0504ca1d040c0800b30
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-10-18 20:42:51 +0300

    int/builder: changes the test to fix redundancy of breadcrumbs and sitemap subtests using node titles for paths by making the sitemap test use hrefs. also changes subtest internal structure to create per-entry subtests for improving debugging

--- internal/builder/build_test.go
+++ internal/builder/build_test.go
@@ -4,38 +4,50 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 	"testing"
 
 	"github.com/ufukty/kask/internal/builder/markdown"
 )
 
-func linearize(n *Node) string {
-	s := n.Title
+// returns the DFS forest
+func forest(n *Node) []*Node {
+	f := []*Node{n}
 	for _, c := range n.Children {
-		s += "\n| " + strings.ReplaceAll(linearize(c), "\n", "\n| ")
+		f = append(f, forest(c)...)
 	}
-	return s
+	return f
 }
 
-func breadcrumbs(n *Node) string {
-	s := ""
-	if n.Parent != nil {
-		s += breadcrumbs(n.Parent)
+func fmap[S, T any](ss []S, m func(S) T) []T {
+	ts := make([]T, len(ss))
+	for i, s := range ss {
+		ts[i] = m(s)
 	}
-	return s + "/" + n.Title
-}
-
-func dfs(n *Node, v func(*Node)) {
-	v(n)
-	for _, c := range n.Children {
-		dfs(c, v)
-	}
+	return ts
 }
 
 func check(tmp, path string) bool {
 	_, err := os.Stat(filepath.Join(tmp, path))
 	return err == nil
+}
+
+func ancestry(n *Node) []*Node {
+	ancestry := []*Node{}
+	for n := n; n != nil; n = n.Parent {
+		ancestry = append(ancestry, n)
+	}
+	slices.Reverse(ancestry)
+	return ancestry
+}
+
+func titlePaths(f []*Node) []string {
+	ss := []string{}
+	for _, n := range f {
+		ss = append(ss, strings.Join(fmap(ancestry(n), func(n *Node) string { return n.Title }), "/"))
+	}
+	return ss
 }
 
 func TestBuild(t *testing.T) {
@@ -84,37 +96,47 @@ func TestBuild(t *testing.T) {
 	})
 
 	t.Run("sitemap", func(t *testing.T) {
-		const expected = `.
-| career
-| Docs
-| | ACME Bird Seed
-| | Download
-| | ACME Magnet
-| | tutorials
-| | | Getting Started
-| products`
-		if got := linearize(b.root3); got != expected {
-			t.Fatalf("expected:\n\n%s\n\ngot:\n%s", expected, got)
+		expected := []string{
+			"/index.html",                          // "."
+			"/career/",                             // "./career",
+			"/docs/",                               // "./Docs",
+			"/docs/birdseed.html",                  // "./Docs/ACME Bird Seed",
+			"/docs/download.html",                  // "./Docs/Download",
+			"/docs/magnet.html",                    // "./Docs/ACME Magnet",
+			"/docs/tutorials/getting-started.html", // "./Docs/tutorials/Getting Started"
+			"/products/",                           // "./products",
+		}
+
+		got := fmap(forest(b.root3), func(n *Node) string { return n.Href })
+		for _, e := range expected {
+			t.Run("sitemap for "+strings.ReplaceAll(e, "/", "\\"), func(t *testing.T) {
+				if !slices.Contains(got, e) {
+					t.Errorf("not found")
+				}
+			})
 		}
 	})
 
 	t.Run("breadcrumbs", func(t *testing.T) {
-		got := ""
-		dfs(b.root3, func(n *Node) {
-			got += "\n" + breadcrumbs(n)
-		})
-		expected := `
-/.
-/./career
-/./Docs
-/./Docs/ACME Bird Seed
-/./Docs/Download
-/./Docs/ACME Magnet
-/./Docs/tutorials
-/./Docs/tutorials/Getting Started
-/./products`
-		if got != expected {
-			t.Fatalf("expected:\n\n%s\n\ngot:\n%s", expected, got)
+		expected := []string{
+			".",
+			"./career",
+			"./Docs",
+			"./Docs/ACME Bird Seed",
+			"./Docs/Download",
+			"./Docs/ACME Magnet",
+			"./Docs/tutorials",
+			"./Docs/tutorials/Getting Started",
+			"./products",
+		}
+
+		got := titlePaths(forest(b.root3))
+		for _, e := range expected {
+			t.Run("sitemap for "+strings.ReplaceAll(e, "/", "\\"), func(t *testing.T) {
+				if !slices.Contains(got, e) {
+					t.Errorf("not found")
+				}
+			})
 		}
 	})
 }
