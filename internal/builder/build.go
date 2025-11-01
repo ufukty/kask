ac22743df6e38973b3bbeccf1a691bc4a72fc1c7
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-11-01 20:59:01 +0300

    int/builders: file organization

--- internal/builder/build.go
+++ internal/builder/build.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"net/url"
 	"os"
 	"path/filepath"
 	"slices"
@@ -98,24 +97,6 @@ type dir2 struct {
 	pagesTmpl     []string // src paths
 	stylesheets   []string // dst paths
 	templates     *template.Template
-}
-
-type paths struct {
-	src string
-	dst string // stripped ordering
-	url string // path encoded
-}
-
-func (p paths) withChild(item string, isToStrip bool) paths {
-	dst := item
-	if isToStrip {
-		dst = stripOrdering(dst)
-	}
-	return paths{
-		src: filepath.Join(p.src, item),
-		dst: filepath.Join(p.dst, dst),
-		url: filepath.Join(p.url, url.PathEscape(dst)),
-	}
 }
 
 func (b *builder) toDir2(d, p *directory.Dir, parent paths) *dir2 {
