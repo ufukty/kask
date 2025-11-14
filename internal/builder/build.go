04827ac10a92362e610d5ad12e65cb810afa29a8
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-11-14 23:32:03 +0300

    int/builder: fixes the `builder.links` insertions canonicalizing the source paths which is now outdated

--- internal/builder/build.go
+++ internal/builder/build.go
@@ -115,7 +115,7 @@ func (b *builder) toDir2(d, p *directory.Dir, parent paths) *dir2 {
 		stylesheets:   nil,
 		templates:     nil,
 	}
-	b.links[canonicalize(d2.paths.src)] = d2.paths.url
+	b.links[d2.paths.src] = d2.paths.url
 	for _, subdir := range d.Subdirs {
 		d2.subdirs = append(d2.subdirs, b.toDir2(subdir, d, ps))
 	}
@@ -267,7 +267,7 @@ func (b *builder) toNode(d *dir2, parent *Node) (*Node, error) {
 				Parent:   n,
 				Children: []*Node{},
 			}
-			b.links[canonicalize(page)] = href
+			b.links[page] = href
 			b.leaves[pageref{d, page}] = c
 			n.Children = append(n.Children, c)
 		}
