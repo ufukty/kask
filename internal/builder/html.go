5c83a0f74c2e278d421c243f7899187fc5c447cc
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-02-11 01:40:18 +0300

    int/builder: changes the template page links rewriter to collect all links with invalid targets instead of returning early

--- internal/builder/html.go
+++ internal/builder/html.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"slices"
+	"strings"
 
 	"github.com/ufukty/kask/internal/builder/rewriter"
 )
@@ -11,6 +12,7 @@ import (
 var anchorHref = regexp.MustCompile(`<a[^>]*href="([^"]*)"[^>]*>[^<]*</a>`)
 
 func rewriteLinksInHtmlPage(rw *rewriter.Rewriter, page string, bs []byte) ([]byte, error) {
+	invTargets := []string{}
 	delta := 0
 	for _, matches := range anchorHref.FindAllSubmatchIndex(bs, -1) {
 		if len(matches) < 4 {
@@ -19,12 +21,18 @@ func rewriteLinksInHtmlPage(rw *rewriter.Rewriter, page string, bs []byte) ([]by
 		start, end := matches[2]+delta, matches[3]+delta
 		m1 := bs[start:end]
 		n, err := rw.Rewrite(string(m1), page)
-		if err != nil {
-			return nil, fmt.Errorf("found the link %q: %w", string(m1), err)
+		if err == rewriter.ErrInvalidTarget {
+			invTargets = append(invTargets, fmt.Sprintf("%q", string(m1)))
+			continue
+		} else if err != nil {
+			return nil, fmt.Errorf("rewriting link %q: %w", string(m1), err)
 		}
 		m2 := []byte(n)
 		bs = slices.Replace(bs, start, end, m2...)
 		delta += len(m2) - len(m1)
+	}
+	if len(invTargets) > 0 {
+		return nil, fmt.Errorf("found links to invalid target(s): %s", strings.Join(invTargets, ", "))
 	}
 	return bs, nil
 }
