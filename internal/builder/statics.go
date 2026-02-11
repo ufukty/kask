63458feae9346e0dd671b7a25d924fc9bf67e446
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-02-12 01:08:50 +0300

    int/builder: whitespace

--- internal/builder/statics.go
+++ internal/builder/statics.go
@@ -75,24 +75,20 @@ func (b *builder) copyAssetsFolders(d *dir2) error {
 		if err != nil {
 			return fmt.Errorf("creating directory: %w", err)
 		}
-
 		dst := filepath.Join(b.args.Dst, d.paths.dst, ".assets")
 		src := filepath.Join(b.args.Src, d.paths.src, ".assets")
 		if b.args.Verbose {
 			fmt.Println("copying", dst)
 		}
-
 		err = copy.Dir(dst, src)
 		if err != nil {
 			return fmt.Errorf("copy dir: %w", err)
 		}
 	}
-
 	for _, subdir := range d.subdirs {
 		if err := b.copyAssetsFolders(subdir); err != nil {
 			return fmt.Errorf("%q: %w", filepath.Base(subdir.paths.src), err)
 		}
 	}
-
 	return nil
 }
