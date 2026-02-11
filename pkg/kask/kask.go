591e63e0d61cb27b57dff27414dc192b0647d7d7
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-02-11 12:52:17 +0300

    pkg/kask: adds example templating technics for sitemaps, breadcrumbs and linking stylesheets. the information kept short to not create second source of truth to the docs yet still providing a concise guide that looks to pkgsite

--- pkg/kask/kask.go
+++ pkg/kask/kask.go
@@ -1,3 +1,5 @@
+// Package kask provides documentation for user-facing Kask code.
+// The information here are supposed to be accessed by Kask users in their templates.
 package kask
 
 import (
@@ -8,6 +10,29 @@ import (
 //   - Unvisitable folder
 //   - Visitable folder (has `index.tmpl` or `README.md` file)
 //   - Page of a `.tmpl` or `.md` file
+//
+// # Printing sitemaps
+//
+// Use a recursive template that calls itself on each children:
+//
+//	{{define "sitemap"}}
+//	  <li>
+//	    <a href="{{.Href}}">{{.Title}}</a>
+//		  {{with .Children}}
+//		  <ul>
+//		    {{range .}}
+//		    {{template "sitemap" .}}
+//		    {{end}}
+//		  </ul>
+//		  {{end}}
+//	  </li>
+//	{{end}}
+//
+// # Printing breadcrumbs
+//
+// Use a recursive template but invoke it with the [TemplateContent.Node],
+// instead of [TemplateContent.Root] and call itself on the parent until
+// parent is `nil`.
 type Node struct {
 	Title    string // Sourced either from the file, meta file or file/folder name
 	Href     string // Visitable when filled
@@ -31,6 +56,17 @@ type Markdown struct {
 
 // TemplateContent provides the dynamic information a template
 // may need to render a page.
+//
+// # Linking stylesheets
+//
+// TemplateContent contains the links for smallest set of Css bundles the
+// current page needs to link. Since Kask bundles stylesheets at each level
+// of directory structure separately, the pages need to link stylesheets
+// created at parent sections when available.
+//
+//	{{range .Stylesheets}}
+//	<link rel="stylesheet" href="{{.}}">
+//	{{end}}
 type TemplateContent struct {
 	Stylesheets []string // Needs to be linked in the page.
 	Node        *Node    // Represents the page currently rendered.
