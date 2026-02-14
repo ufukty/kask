c299b839ddba6c305b2a884fabcc0371790961e1
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-02-14 15:23:32 +0300

    int/builder: updates the allocation check test to report back the new input size after doubling the docs site proxy

--- internal/builder/build_test.go
+++ internal/builder/build_test.go
@@ -2,7 +2,7 @@ package builder
 
 import (
 	"fmt"
-	"math"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -313,58 +313,69 @@ func ExampleBuilder_mdLinkReplacements() {
 	// <a href="/a/tmpl.html#Title">sibling</a>
 }
 
-func docSiteOfSize(dst, mid, src string, size int) error {
-	entries, err := os.ReadDir(src)
-	if err != nil {
-		return fmt.Errorf("os.ReadDir: %w", err)
-	}
-	for _, entry := range entries {
-		if entry.IsDir() && !strings.HasPrefix(entry.Name(), ".") {
-			for i := range size {
-				dst2 := filepath.Join(dst, entry.Name()+strconv.Itoa(i))
-				src2 := filepath.Join(src, entry.Name())
-				err := copy.Dir(dst2, src2)
-				if err != nil {
-					return fmt.Errorf("duplication %d: copy.Dir(%q, %q): %w", i, dst2, src2, err)
-				}
-			}
-		} else if entry.IsDir() {
-			dst2 := filepath.Join(dst, entry.Name())
-			src2 := filepath.Join(src, entry.Name())
-			err := copy.Dir(dst2, src2)
-			if err != nil {
-				return fmt.Errorf("copy.Dir(%q, %q): %w", dst2, src2, err)
-			}
-		} else {
-			dst2 := filepath.Join(dst, entry.Name())
-			src2 := filepath.Join(src, entry.Name())
-			err := copy.File(dst2, src2)
-			if err != nil {
-				return fmt.Errorf("copy.File(%q, %q): %w", dst2, src2, err)
-			}
+func dirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.WalkDir(path, func(_ string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
 		}
+		if !d.IsDir() {
+			info, err := d.Info()
+			if err != nil {
+				return err
+			}
+			size += info.Size()
+		}
+		return nil
+	})
+	return size, err
+}
+
+// it duplicates the content folder root as a section
+//
+//	docs    docs
+//	|-a     |-a
+//	|-b  => |-b
+//	        |-docs
+//	          |-a
+//	          |-b
+func doubleUpContentFolder(path string) (float64, error) {
+	copy.Dir(filepath.Join(path, "docs-again"), path)
+	s, err := dirSize(path)
+	if err != nil {
+		return -1, fmt.Errorf("dirSize: %w", err)
 	}
-	return nil
+	return float64(s), nil
+}
+
+func toPythonArraySyntax(vs []float64) string {
+	ss := []string{}
+	for _, v := range vs {
+		ss = append(ss, strconv.FormatFloat(v, 'f', 2, 64))
+	}
+	return fmt.Sprintf("[%s]", strings.Join(ss, ", "))
 }
 
 // TestBuilder_docsSite performs a series of builds with the docs site which
 // its pages are de-duplicated 2*i times each run to check if the memory
 // consumption scales sublinear.
 func TestBuilder_docsSiteAllocationScaling(t *testing.T) {
-	tmp := []string{}
-	for range 21 {
-		tmp = append(tmp, t.TempDir())
+	tmp := t.TempDir()
+	fmt.Println(tmp)
+	err := copy.Dir(tmp, "../../docs")
+	if err != nil {
+		t.Errorf("prep, copying docs site into the test directory to recursively duplicate its contents: %v", err)
 	}
-	f, err := scales.Allocations(1024, func(size int) error {
-		i := int(math.Log2(float64(size)))
-		return docSiteOfSize(tmp[i], tmp[i+10], "../../docs", i)
-	}, func(size int) error {
-		i := int(math.Log2(float64(size)))
-		return Build(Args{Src: tmp[i], Dst: tmp[i+10], Domain: "/"})
+	f, a, err := scales.Allocations(20, func() (float64, error) {
+		return doubleUpContentFolder(tmp)
+	}, func() error {
+		return Build(Args{Src: tmp, Dst: t.TempDir(), Domain: "/"})
 	})
 	if err != nil {
 		t.Errorf("act, unexpected error: %v", err)
-	} else if f == scales.Superlinear {
+	} else if f == scales.NonSublinear {
+		fmt.Printf("x=%s\n", toPythonArraySyntax(a.Sizes))
+		fmt.Printf("y=%s\n", toPythonArraySyntax(a.Allocs))
 		t.Fatal("assert memory allocation can't scale superlinear")
 	}
 }
