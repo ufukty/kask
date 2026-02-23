168d4ba35b9c31900e42006c2fc81e516fd99da6
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-02-23 14:30:01 +0300

    int/rewriter: multiplies the test cases for observing issues where a real domain is used

--- internal/rewriter/rewrite_test.go
+++ internal/rewriter/rewrite_test.go
@@ -59,7 +59,7 @@ func rewriter() *Rewriter {
 		"a/b":      "/a/b/",
 		"a/b/c /d": "/a/b/c%20/d/",
 	}
-	r := New(paths.Paths{Src: ".", Dst: ".", Url: "/"})
+	r := New()
 	for src, url := range links {
 		r.Bank(src, url)
 	}
@@ -259,6 +259,179 @@ func TestRewrite_Rewrite_linksToUnvisitableDirs(t *testing.T) {
 
 func TestRewrite_Rewrite_linksToUnexistingNodes(t *testing.T) {
 	linker := paths.Paths{Src: "a/b/page.tmpl", Dst: "a/b/page.html", Url: "/a/b/page.html"}
+	tcs := []string{
+		"../x",                    // /x/
+		"../../x.tmpl",            // /x.html
+		"../../..",                // path escape
+		"../../../..",             // path escape
+		"a/b/c/../../../../../..", // path escape
+	}
+	rw := rewriter()
+	for _, input := range tcs {
+		t.Run(testname(input), func(t *testing.T) {
+			got, err := rw.Rewrite(input, linker)
+			if err == nil {
+				t.Errorf("act, unexpected success with value: %s", got)
+			}
+		})
+	}
+}
+
+// TODO: add cases where (linker ⋁ linked) (has ⋁ should contain) encoded parts
+func TestRewrite_Rewrite_toVisitableDirWithDomain(t *testing.T) {
+	d0 := paths.Paths{Src: "page.tmpl", Dst: "page.html", Url: "https://kask.ufukty.com/page.html"}
+	tcs := map[tc]string{
+		{linker: d0, linked: "/"}:                       "https://kask.ufukty.com/",
+		{linker: d0, linked: "/a/b"}:                    "https://kask.ufukty.com/a/b/",
+		{linker: d0, linked: "/a/b/index.tmpl"}:         "https://kask.ufukty.com/a/b/",
+		{linker: d0, linked: "/README.md"}:              "https://kask.ufukty.com/",
+		{linker: d0, linked: "a/../a/b"}:                "https://kask.ufukty.com/a/b/",
+		{linker: d0, linked: "a/../a/b/c /d"}:           "https://kask.ufukty.com/a/b/c%20/d/",
+		{linker: d0, linked: "a/../a/b/c /d/README.md"}: "https://kask.ufukty.com/a/b/c%20/d/",
+		{linker: d0, linked: "a/b"}:                     "https://kask.ufukty.com/a/b/",
+		{linker: d0, linked: "a/b/c /d"}:                "https://kask.ufukty.com/a/b/c%20/d/",
+		{linker: d0, linked: "a/b/c /d/README.md"}:      "https://kask.ufukty.com/a/b/c%20/d/",
+	}
+	rw := rewriter()
+	for tc, te := range sorted(tcs) {
+		t.Run(fmt.Sprintf("%s=>%s", testname(tc.linker.Src), testname(tc.linked)), func(t *testing.T) {
+			got, err := rw.Rewrite(tc.linked, tc.linker)
+			if err != nil {
+				t.Errorf("act, unexpected error: %v", err)
+			} else if te != got {
+				t.Errorf("expected %q got %q", te, got)
+			}
+		})
+	}
+}
+
+func TestRewrite_Rewrite_toPageURLsWithDomain(t *testing.T) {
+	d0 := paths.Paths{Src: "page.tmpl", Dst: "page.html", Url: "https://kask.ufukty.com/page.html"}
+	d2 := paths.Paths{Src: "a/b/page.tmpl", Dst: "a/b/page.html", Url: "https://kask.ufukty.com/a/b/page.html"}
+	tcs := map[tc]string{
+		{linker: d0, linked: "/a/b/"}:          "https://kask.ufukty.com/a/b/",
+		{linker: d0, linked: "/"}:              "https://kask.ufukty.com/",
+		{linker: d0, linked: "a/../a/b/c /d/"}: "https://kask.ufukty.com/a/b/c%20/d/",
+		{linker: d0, linked: "a/b/c /d/"}:      "https://kask.ufukty.com/a/b/c%20/d/",
+		{linker: d2, linked: "../../"}:         "https://kask.ufukty.com/",
+		{linker: d2, linked: "./../../"}:       "https://kask.ufukty.com/",
+	}
+	rw := rewriter()
+	for tc, te := range sorted(tcs) {
+		t.Run(fmt.Sprintf("%s=>%s", testname(tc.linker.Src), testname(tc.linked)), func(t *testing.T) {
+			got, err := rw.Rewrite(tc.linked, tc.linker)
+			if err != nil {
+				t.Errorf("act, unexpected error: %v", err)
+			} else if te != got {
+				t.Errorf("expected %q got %q", te, got)
+			}
+		})
+	}
+}
+
+func TestRewrite_Rewrite_toPageWithDomain(t *testing.T) {
+	d0 := paths.Paths{Src: "page.tmpl", Dst: "page.html", Url: "https://kask.ufukty.com/page.html"}
+	d2 := paths.Paths{Src: "a/b/page.tmpl", Dst: "a/b/page.html", Url: "https://kask.ufukty.com/a/b/page.html"}
+	tcs := map[tc]string{
+		{linker: d0, linked: "/page.md"}:           "https://kask.ufukty.com/page.html",
+		{linker: d0, linked: "a/../a/b/page.tmpl"}: "https://kask.ufukty.com/a/b/page.html",
+		{linker: d0, linked: "a/b/page.tmpl"}:      "https://kask.ufukty.com/a/b/page.html",
+		{linker: d2, linked: "../../page.md"}:      "https://kask.ufukty.com/page.html",
+		{linker: d2, linked: "./../../page.md"}:    "https://kask.ufukty.com/page.html",
+	}
+	rw := rewriter()
+	for tc, te := range sorted(tcs) {
+		t.Run(fmt.Sprintf("%s=>%s", testname(tc.linker.Src), testname(tc.linked)), func(t *testing.T) {
+			got, err := rw.Rewrite(tc.linked, tc.linker)
+			if err != nil {
+				t.Errorf("act, unexpected error: %v", err)
+			} else if te != got {
+				t.Errorf("expected %q got %q", te, got)
+			}
+		})
+	}
+}
+
+func TestRewrite_Rewrite_toVisitableDirAnchorWithDomain(t *testing.T) {
+	d0 := paths.Paths{Src: "page.tmpl", Dst: "page.html", Url: "https://kask.ufukty.com/page.html"}
+	tcs := map[tc]string{
+		{linker: d0, linked: "/#title"}:                  "https://kask.ufukty.com/#title",
+		{linker: d0, linked: "/a/b/#title"}:              "https://kask.ufukty.com/a/b/#title",
+		{linker: d0, linked: "/a/b/index.tmpl#title"}:    "https://kask.ufukty.com/a/b/#title",
+		{linker: d0, linked: "/a/b#title"}:               "https://kask.ufukty.com/a/b/#title",
+		{linker: d0, linked: "/README.md#title"}:         "https://kask.ufukty.com/#title",
+		{linker: d0, linked: "a/b/#title"}:               "https://kask.ufukty.com/a/b/#title",
+		{linker: d0, linked: "a/b/c /d/#title"}:          "https://kask.ufukty.com/a/b/c%20/d/#title",
+		{linker: d0, linked: "a/b/c /d/README.md#title"}: "https://kask.ufukty.com/a/b/c%20/d/#title",
+		{linker: d0, linked: "a/b/index.tmpl#title"}:     "https://kask.ufukty.com/a/b/#title",
+	}
+	rw := rewriter()
+	for tc, te := range sorted(tcs) {
+		t.Run(fmt.Sprintf("%s=>%s", testname(tc.linker.Src), testname(tc.linked)), func(t *testing.T) {
+			got, err := rw.Rewrite(tc.linked, tc.linker)
+			if err != nil {
+				t.Errorf("act, unexpected error: %v", err)
+			} else if te != got {
+				t.Errorf("expected %q got %q", te, got)
+			}
+		})
+	}
+}
+
+func TestRewrite_Rewrite_toPageAnchorWithDomain(t *testing.T) {
+	d0 := paths.Paths{Src: "page.tmpl", Dst: "page.html", Url: "https://kask.ufukty.com/page.html"}
+	d2 := paths.Paths{Src: "a/b/page.tmpl", Dst: "a/b/page.html", Url: "https://kask.ufukty.com/a/b/page.html"}
+	tcs := map[tc]string{
+		{linker: d0, linked: "#"}:                        "https://kask.ufukty.com/page.html#",
+		{linker: d0, linked: "#title"}:                   "https://kask.ufukty.com/page.html#title",
+		{linker: d0, linked: "a/../a/b/page.tmpl#title"}: "https://kask.ufukty.com/a/b/page.html#title",
+		{linker: d0, linked: "a/b/c /page.md#title"}:     "https://kask.ufukty.com/a/b/c%20/page.html#title",
+		{linker: d0, linked: "a/b/page.tmpl#title"}:      "https://kask.ufukty.com/a/b/page.html#title",
+		{linker: d0, linked: "page.md#title"}:            "https://kask.ufukty.com/page.html#title",
+		{linker: d2, linked: "./../../page.md#title"}:    "https://kask.ufukty.com/page.html#title",
+		{linker: d2, linked: "/page.md#title"}:           "https://kask.ufukty.com/page.html#title",
+		{linker: d2, linked: "#"}:                        "https://kask.ufukty.com/a/b/page.html#",
+		{linker: d2, linked: "#title"}:                   "https://kask.ufukty.com/a/b/page.html#title",
+	}
+	rw := rewriter()
+	for tc, te := range sorted(tcs) {
+		t.Run(fmt.Sprintf("%s=>%s", testname(tc.linker.Src), testname(tc.linked)), func(t *testing.T) {
+			got, err := rw.Rewrite(tc.linked, tc.linker)
+			if err != nil {
+				t.Errorf("act, unexpected error: %v", err)
+			} else if te != got {
+				t.Errorf("expected %q got %q", te, got)
+			}
+		})
+	}
+}
+
+func TestRewrite_Rewrite_linksToUnvisitableDirsWithDomain(t *testing.T) {
+	linker := paths.Paths{Src: "a/b/page.tmpl", Dst: "a/b/page.html", Url: "https://kask.ufukty.com/a/b/page.html"}
+	tcs := []string{
+		"..",      // /a/
+		"../",     // /a/
+		"../b/c",  // /a/b/c%20/
+		"../b/c/", // /a/b/c%20/
+		"./../",   // /a/
+		"./c",     // /a/b/c%20/
+		"./c/",    // /a/b/c%20/
+		"c",       // /a/b/c%20/
+		"c/",      // /a/b/c%20/
+	}
+	rw := rewriter()
+	for _, input := range tcs {
+		t.Run(testname(input), func(t *testing.T) {
+			_, err := rw.Rewrite(input, linker)
+			if err == nil {
+				t.Errorf("act, unexpected success")
+			}
+		})
+	}
+}
+
+func TestRewrite_Rewrite_linksToUnexistingNodesWithDomain(t *testing.T) {
+	linker := paths.Paths{Src: "a/b/page.tmpl", Dst: "a/b/page.html", Url: "https://kask.ufukty.com/a/b/page.html"}
 	tcs := []string{
 		"../x",                    // /x/
 		"../../x.tmpl",            // /x.html
