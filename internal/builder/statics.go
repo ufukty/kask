646a7e9756fbe2cd4d6fead22ca1b603ebf35c04
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-02-26 22:02:25 +0300

    int/builder: package organization

--- internal/builder/statics.go
+++ internal/builder/statics.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 
 	"go.ufukty.com/kask/internal/builder/bundle"
-	"go.ufukty.com/kask/internal/builder/copy"
 )
 
 func (b *builder) write(dst, content string) error {
@@ -66,29 +65,5 @@ func (b *builder) bundleAndPropagateStylesheets(d *dir2, toPropagate []string) e
 		}
 	}
 
-	return nil
-}
-
-func (b *builder) copyAssetsFolders(d *dir2) error {
-	if d.original.Assets {
-		err := os.MkdirAll(filepath.Join(b.args.Dst, d.paths.Dst), 0o755)
-		if err != nil {
-			return fmt.Errorf("creating directory: %w", err)
-		}
-		dst := filepath.Join(b.args.Dst, d.paths.Dst, ".assets")
-		src := filepath.Join(b.args.Src, d.paths.Src, ".assets")
-		if b.args.Verbose {
-			fmt.Println("copying", dst)
-		}
-		err = copy.Dir(dst, src)
-		if err != nil {
-			return fmt.Errorf("copy dir: %w", err)
-		}
-	}
-	for _, subdir := range d.subdirs {
-		if err := b.copyAssetsFolders(subdir); err != nil {
-			return fmt.Errorf("%q: %w", filepath.Base(subdir.paths.Src), err)
-		}
-	}
 	return nil
 }
