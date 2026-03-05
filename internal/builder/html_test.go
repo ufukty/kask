5d7749deae6143ee28c0234eecc75bf611ca7c62
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-03-05 19:50:35 +0300

    int/builder: changes the `htmlPostProcess` test for testing cases in the first extension of link rewriting targets #121

--- internal/builder/html_test.go
+++ internal/builder/html_test.go
@@ -32,19 +32,23 @@ func fixture() *builder {
 		"a/b/page.md":   "https://kask.ufukty.com/a/b/page.html",
 
 		// visitable dirs:
+		".":   "https://kask.ufukty.com/",
 		"a/":  "https://kask.ufukty.com/a/",
 		"a/b": "https://kask.ufukty.com/a/b/",
 
 		// assets
+		".assets/font.woff2":   "https://kask.ufukty.com/.assets/font.woff2",
 		"a/.assets/img.jpg":    "https://kask.ufukty.com/a/.assets/img.jpg",
 		"a/.assets/img@2x.jpg": "https://kask.ufukty.com/a/.assets/img%402x.jpg",
 		"a/.assets/img@3x.jpg": "https://kask.ufukty.com/a/.assets/img%403x.jpg",
 		"a/.assets/poster.jpg": "https://kask.ufukty.com/a/.assets/poster.jpg",
+		"a/.assets/video.mp4":  "https://kask.ufukty.com/a/.assets/video.mp4",
+		"a/.assets/og.jpg":     "https://kask.ufukty.com/a/.assets/og.jpg",
 	}
 	for s, u := range m {
 		rw.Bank(s, u)
 	}
-	return &builder{rw: rw, incorrectLinks: map[string][]string{}} // TODO: remove til commit
+	return &builder{rw: rw}
 }
 
 func TestBuilder_htmlPostProcess(t *testing.T) {
@@ -67,6 +71,38 @@ func TestBuilder_htmlPostProcess(t *testing.T) {
 		"img src and srcset": {
 			input:    `<img src=".assets/img.jpg" srcset=".assets/img@2x.jpg 2x, .assets/img@3x.jpg 3x">`,
 			expected: `<img src="https://kask.ufukty.com/a/.assets/img.jpg" srcset="https://kask.ufukty.com/a/.assets/img%402x.jpg 2x, https://kask.ufukty.com/a/.assets/img%403x.jpg 3x">`,
+		},
+		"link href to domain": {
+			input:    `<link rel="canonical" href="/" />`,
+			expected: `<link rel="canonical" href="https://kask.ufukty.com" />`,
+		},
+		"link href to asset with additional attributes": {
+			input:    `<link rel="preload" href="/.assets/font.woff2" as="font" />`,
+			expected: `<link rel="preload" href="https://kask.ufukty.com/.assets/font.woff2" as="font" />`,
+		},
+		"meta tag for og:image": {
+			input:    `<meta property="og:image" content=".assets/og.jpg" />`,
+			expected: `<meta property="og:image" content="https://kask.ufukty.com/a/.assets/og.jpg" />`,
+		},
+		"meta tag for og:url": {
+			input:    `<meta property="og:url" content="." />`,
+			expected: `<meta property="og:url" content="https://kask.ufukty.com/a/page.html" />`,
+		},
+		"meta tag for twitter:image": {
+			input:    `<meta name="twitter:image" content=".assets/og.jpg" />`,
+			expected: `<meta name="twitter:image" content="https://kask.ufukty.com/a/.assets/og.jpg" />`,
+		},
+		"meta tag for twitter:url": {
+			input:    `<meta name="twitter:image" content="." />`,
+			expected: `<meta name="twitter:image" content="https://kask.ufukty.com/a/page.html" />`,
+		},
+		"iframe src": {
+			input:    `<iframe src=".assets/embeded-player.html"></iframe>`,
+			expected: `<iframe src="https://kask.ufukty.com/.assets/embedded-player.html"></iframe>`,
+		},
+		"video and source": {
+			input:    `<video poster=".assets/poster.jpg"><source src=".assets/video.mp4" type="video/mp4"></video>`,
+			expected: `<video poster="https://kask.ufukty.com/a/.assets/poster.jpg"><source src="https://kask.ufukty.com/a/.assets/video.mp4" type="video/mp4"></video>`,
 		},
 	}
 	b := fixture()
