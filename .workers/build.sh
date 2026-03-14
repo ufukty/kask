888a4b8f53ac0aa6a06f5c36a15f898d6b99d3b2
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-03-14 20:46:49 +0300

    .wo/build.sh: changes to use local directory to install Kask into and build from which eliminates the need to find `GOBIN` (which was `/opt/buildhome/.asdf/installs/golang/1.24.3/bin`) or `GOPATH`.

--- .workers/build.sh
+++ .workers/build.sh
@@ -8,8 +8,5 @@ if test "$WORKERS_CI_BRANCH" = "main"; then
   git checkout "$(git tag --list 'v*' --sort '-version:refname' | head -n 1)"
 fi
 
-go install ./cmd/kask
-go env
-echo "$PATH" | sed 's/:/\n/g' | sort
-PATH="$(go env GOBIN):$(go env GOPATH)/bin:${PATH}"
-kask build -in docs -out docs-build -domain "https://kask.ufukty.com" -v -cfw
+GOBIN="$PWD" go install ./cmd/kask # embed version
+./kask build -in docs -out docs-build -domain "https://kask.ufukty.com" -v -cfw
