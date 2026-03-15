086ba1ec622b43802fd5e1033fbaf87e7827d053
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-03-15 11:54:30 +0300

    .wo/build.sh: updates for new install path.

--- .workers/build.sh
+++ .workers/build.sh
@@ -8,5 +8,5 @@ if test "$WORKERS_CI_BRANCH" = "main"; then
   git checkout "$(git tag --list 'v*' --sort '-version:refname' | head -n 1)"
 fi
 
-GOBIN="$PWD" go install ./cmd/kask # embed version
+GOBIN="$PWD" go install . # embed version
 ./kask build -in docs -out docs-build -domain "https://kask.ufukty.com" -v -cfw
