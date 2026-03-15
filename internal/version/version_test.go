cea1e5acbb95c8f8c993470a5025cb592bfb3eab
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-03-15 11:48:11 +0300

    int/version: moves in the test file

--- internal/version/version_test.go
+++ internal/version/version_test.go
@@ -1,19 +1,15 @@
 package version
 
 import (
-	"fmt"
 	"testing"
-
-	"go.ufukty.com/kask/internal/version"
 )
 
-func TestDigBuildInfo(t *testing.T) {
-	v, err := version.OfBuild()
+func TestOfBuild(t *testing.T) {
+	v, err := OfBuild()
 	if err != nil {
 		t.Errorf("act, unexpected error: %v", err)
 	}
-	if v == "" {
+	if v != "(devel)" {
 		t.Errorf("assert, unexpected empty value")
 	}
-	fmt.Println(v)
 }
