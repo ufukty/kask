ff18a5745cbc9cae2787e2c08b4e425803e313e8
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-03-21 12:34:24 +0300

    int/disk: adds commentary on the split of two interfaces.

--- internal/disk/disk.go
+++ internal/disk/disk.go
@@ -5,15 +5,20 @@ import (
 	"io/fs"
 )
 
-type ReadFS interface {
-	fs.ReadFileFS
-	fs.ReadDirFS
-	fs.StatFS
-}
-
-// Writable [fs.FS] for unit testing.
-type WriteFS interface {
-	Create(name string) (io.WriteCloser, error)
-	MkdirAll(path string) error
-	WriteFile(path string, data []byte) error
-}
+// [ReadFS] needs to conform [fs.FS] to work with [templates.Template.ParseFS]
+// and other stdlib symbols.
+// Which is unfortunate because [fs.File] is read-only.
+// [WriteFS] is needed to make units testable.
+// Hence, they are split into two interfaces.
+type (
+	ReadFS interface {
+		fs.ReadFileFS
+		fs.ReadDirFS
+		fs.StatFS
+	}
+	WriteFS interface {
+		Create(name string) (io.WriteCloser, error)
+		MkdirAll(path string) error
+		WriteFile(path string, data []byte) error
+	}
+)
