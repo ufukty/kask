a357f6e159e80456446f1aad1ab7914512f574c9
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-03-29 08:23:04 +0300

    int/disk/memory: fixes a test name and its location.

--- internal/disk/memory/memory_test.go
+++ internal/disk/memory/memory_test.go
@@ -227,3 +227,29 @@ func TestDir_ReadFile(t *testing.T) {
 
 	assert.Results(t, string(expected), string(got))
 }
+
+func TestDir_ReadAll(t *testing.T) {
+	d := New()
+	expected := "hello world"
+
+	t.Run("write", func(t *testing.T) {
+		err := d.WriteFile("a.txt", []byte(expected))
+		if err != nil {
+			t.Errorf("act: %v", err)
+		}
+	})
+
+	var got []byte
+	t.Run("read", func(t *testing.T) {
+		f, err := d.Open("a.txt")
+		if err != nil {
+			t.Errorf("prep: %v", err)
+		}
+		got, err = io.ReadAll(f)
+		if err != nil {
+			t.Fatalf("act, ReadAll: %v", err)
+		}
+	})
+
+	assert.Results(t, expected, string(got))
+}
