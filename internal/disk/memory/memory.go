b8138ffa8a50b01d03f81444ac0899c252f23e68
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-03-29 22:00:41 +0300

    int/disk/memory: adds package doc.

---
+++ internal/disk/memory/memory.go
@@ -0,0 +1,5 @@
+// Package memory contains a file system implementation [Dir] stores data on
+// memory, and its utilities that together allow its use with the standard
+// library utilities such as [fs.WalkDir] or [template.Template]. It is 
+// intended to be used in testing only.
+package memory
