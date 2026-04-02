476a3908436a498409afca0c980b4fc726933a8c
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-04-02 12:13:47 +0300

    int/disk: changes the interface spec to align WriteFS rules to `fs` constraints; mainly the `fs.ValidPath` enforcement and the per-file file permission accounting. see #176

--- internal/disk/disk.go
+++ internal/disk/disk.go
@@ -5,23 +5,23 @@ import (
 	"io/fs"
 )
 
-// [ReadFS] needs to conform [fs.FS] to work with [templates.Template.ParseFS]
-// and other stdlib symbols.
-// Which is unfortunate because [fs.File] is read-only.
-// [WriteFS] is needed to make units testable.
-// Hence, they are split into two interfaces.
-type (
-	ReadFS interface {
-		fs.ReadFileFS
-		fs.ReadDirFS
-		fs.StatFS
-	}
-	WriteFS interface {
-		// As in [os.Create]
-		Create(name string) (io.WriteCloser, error)
-		// As in [os.MkdirAll]
-		MkdirAll(path string) error
-		// As in [os.WriteFile]
-		WriteFile(path string, data []byte) error
-	}
-)
+// [ReadFS] implementations need to guarantee interoperability with standard
+// library utilities such as [fs.WalkDir] and [template.template.ParseFS].
+type ReadFS interface {
+	fs.FS
+	fs.ReadFileFS
+	fs.ReadDirFS
+	fs.StatFS
+}
+
+// [WriteFS] implementations need to guarantee interoperability with the builder
+// package. Methods need to share the same constraints with the [fs.ValidPath]
+// on inputs.
+type WriteFS interface {
+	// As in [os.Create]
+	Create(name string) (io.WriteCloser, error)
+	// As in [os.MkdirAll]
+	MkdirAll(path string, perm fs.FileMode) error
+	// As in [os.WriteFile]
+	WriteFile(path string, data []byte, perm fs.FileMode) error
+}
