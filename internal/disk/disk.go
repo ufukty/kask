f6fe7e22c5856b453e45f74d49bfe0e7cf5df563
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-04-02 13:26:49 +0300

    int/disk: changes the `WriteFS.Create` to return a `fs`-compatible handle.

--- internal/disk/disk.go
+++ internal/disk/disk.go
@@ -14,11 +14,17 @@ type ReadFS interface {
 	fs.StatFS
 }
 
+// TODO: remove once the [fs.File] added a [Write] and update [WriteFS.Create].
+type File interface {
+	fs.File
+	io.Writer
+}
+
 // [WriteFS] implementations need to guarantee interoperability with the builder
 // package. Methods need to share the same constraints with the [fs.ValidPath]
 // on inputs.
 type WriteFS interface {
-	Create(name string) (io.WriteCloser, error)                 // As in [os.Create]
+	Create(name string) (File, error)                           // As in [os.Create].
 	MkdirAll(path string, perm fs.FileMode) error               // As in [os.MkdirAll]
 	WriteFile(path string, data []byte, perm fs.FileMode) error // As in [os.WriteFile]
 }
