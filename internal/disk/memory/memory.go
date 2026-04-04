a9083c669fcd6a2c97c85899a7b5b5a09b998772
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-04-04 23:49:46 +0300

    int/disk/memory: adds to package doc on concurrency.

--- internal/disk/memory/memory.go
+++ internal/disk/memory/memory.go
@@ -1,3 +1,4 @@
 // Package memory provides an in-memory file system implementation [Dir]
 // that is compatible with the fs and disk package interfaces.
+// Not intended for concurrent uses.
 package memory
